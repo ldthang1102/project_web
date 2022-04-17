2214920fec429152d6a2784e6827fa2d82428def
ldthang1102 <thangldgch18812@fpt.edu.vn>
2022-04-17 22:51:33 +0700

    done-project

---
+++ ASMWebTest1Project/Models/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASMWebTest1Project.Models
+{
+    public class Account
+    {
+        
+        [Key]
+        [Required]
+       
+
+
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+
+    }
+}
